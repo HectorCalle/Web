2247591d6ab9690b19c3068736b07c5e6bb8a9ec
HectorCalle <calle.salinas.hector@itdonbosco.org>
2025-05-06 12:38:39 -0400

    actualizacion10

--- app.py
+++ app.py
@@ -6,15 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return "<h1><strong>Esta es la página de About.</strong></h1><p>Esta es una página de ejemplo para mostrar cómo funciona Flask.</p>"
-
-@app.route('/tasks')
-def list_tasks():
-    tareas = ["Lavar la ropa", "Limpiar la casa", "Hacer la compra", "Estudiar para el examen", "Hacer ejercicio", "Leer un libro"]
-    return render_template('tasks.html', tareas=tareas)
-
 @app.route('/login')
 def login():
     return render_template('login.html')
@@ -23,13 +14,25 @@ def login():
 def signup():
     return render_template('signup.html')
 
-@app.route('/create_tasks')
-def create_tasks():
-    return render_template('create_tasks.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/tasks')
+def list_tasks():
+    tareas = ["Lavar la ropa", "Limpiar la casa", "Hacer la compra", "Estudiar para el examen", "Hacer ejercicio", "Leer un libro"]
+    return render_template('tasks.html', tareas=tareas)
 
 @app.route('/task')
-def task():
+def view_task():
     return render_template('task.html')
+
+@app.route('/task/create')
+def create_task():
+    return render_template('create_task.html')
+#Crear una ruta y la vista correspondiente para renderizar un html llamado "create_task.html"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5001)
